9af1e9e96c24a1133c85285bb944b996fb3e0ca9
JeremieMelo <jqgu@utexas.edu>
2025-11-07 13:27:51 -0700

    format

--- core/fdfd/pydiso_solver.py
+++ core/fdfd/pydiso_solver.py
@@ -1,53 +1,54 @@
-'''
-Description: 
+"""
+Description:
 Author: Jiaqi Gu (jiaqigu@asu.edu)
 Date: 2025-11-05 21:21:42
 LastEditors: Jiaqi Gu (jiaqigu@asu.edu)
 LastEditTime: 2025-11-05 21:23:10
 FilePath: /MAPS_local/core/fdfd/pydiso_solver.py
-'''
+"""
+
+import warnings
+
 import numpy as np
 import scipy.sparse as sp
 from pydiso._mkl_solver import (
+    PardisoError,
+    PardisoWarning,
+    _err_messages,
     _PardisoHandle_int_t,
     _PardisoHandle_long_t,
-    get_mkl_int_size,
     get_mkl_int64_size,
+    get_mkl_int_size,
     get_mkl_max_threads,
     get_mkl_pardiso_max_threads,
-    set_mkl_threads,
-    set_mkl_pardiso_threads,
     get_mkl_version,
-    _err_messages,
-    PardisoWarning,
-    PardisoError,
+    set_mkl_pardiso_threads,
+    set_mkl_threads,
 )
-import warnings
 
-
-MATRIX_TYPES ={
-    'real_structurally_symmetric': 1,
-    'real_symmetric_positive_definite': 2,
-    'real_symmetric_indefinite': -2,
-    'complex_structurally_symmetric': 3,
-    'complex_hermitian_positive_definite': 4,
-    'complex_hermitian_indefinite': -4,
-    'complex_symmetric': 6,
-    'real_nonsymmetric': 11,
-    'complex_nonsymmetric': 13}
+MATRIX_TYPES = {
+    "real_structurally_symmetric": 1,
+    "real_symmetric_positive_definite": 2,
+    "real_symmetric_indefinite": -2,
+    "complex_structurally_symmetric": 3,
+    "complex_hermitian_positive_definite": 4,
+    "complex_hermitian_indefinite": -4,
+    "complex_symmetric": 6,
+    "real_nonsymmetric": 11,
+    "complex_nonsymmetric": 13,
+}
 """dict : matrix type string keys and corresponding integer value to describe
 the different supported matrix types.
 """
 
 
-class PardisoTypeConversionWarning(
-        PardisoWarning, sp.SparseEfficiencyWarning):
+class PardisoTypeConversionWarning(PardisoWarning, sp.SparseEfficiencyWarning):
     pass
 
-class MKLPardisoSolver:
 
+class MKLPardisoSolver:
     def __init__(self, A, matrix_type=None, factor=True, verbose=False):
-        '''An interface to the Intel MKL pardiso sparse matrix solver.
+        """An interface to the Intel MKL pardiso sparse matrix solver.
 
         This is a solver class for a scipy sparse matrix using the Pardiso sparse
         solver in the Intel Math Kernel Library.
@@ -101,9 +102,11 @@ class MKLPardisoSolver:
         >>> x_solved = Ainv.solve(b)
         >>> np.allclose(x, x_solved)
         True
-        '''
+        """
         if not sp.issparse(A):
-            raise TypeError(f"type(A)={type(A).__name__} must be a sparse array or sparse matrix.")
+            raise TypeError(
+                f"type(A)={type(A).__name__} must be a sparse array or sparse matrix."
+            )
 
         if A.ndim != 2:
             raise ValueError(f"A.ndim={A.ndim} must be to 2.")
@@ -114,11 +117,11 @@ class MKLPardisoSolver:
         self.shape = n_row, n_col
 
         data_dtype = A.dtype
-        if not(
-            np.issubdtype(data_dtype, np.single) or
-            np.issubdtype(data_dtype, np.double) or
-            np.issubdtype(data_dtype, np.csingle) or
-            np.issubdtype(data_dtype, np.cdouble)
+        if not (
+            np.issubdtype(data_dtype, np.single)
+            or np.issubdtype(data_dtype, np.double)
+            or np.issubdtype(data_dtype, np.csingle)
+            or np.issubdtype(data_dtype, np.cdouble)
         ):
             raise ValueError(
                 f"Unrecognized matrix data type, {data_dtype}. Must be single or double precision of real or complex values."
@@ -129,12 +132,12 @@ class MKLPardisoSolver:
 
         if matrix_type is None:
             if is_complex:
-                matrix_type = MATRIX_TYPES['complex_nonsymmetric']
+                matrix_type = MATRIX_TYPES["complex_nonsymmetric"]
             else:
-                matrix_type = MATRIX_TYPES['real_nonsymmetric']
+                matrix_type = MATRIX_TYPES["real_nonsymmetric"]
 
-        if not(matrix_type in MATRIX_TYPES or matrix_type in MATRIX_TYPES.values()):
-            raise TypeError(f'Unrecognized matrix_type: {matrix_type}')
+        if not (matrix_type in MATRIX_TYPES or matrix_type in MATRIX_TYPES.values()):
+            raise TypeError(f"Unrecognized matrix_type: {matrix_type}")
         if matrix_type in MATRIX_TYPES:
             matrix_type = MATRIX_TYPES[matrix_type]
 
@@ -157,7 +160,9 @@ class MKLPardisoSolver:
         mkl_int_size = get_mkl_int_size()
         mkl_int64_size = get_mkl_int64_size()
 
-        target_int_size = mkl_int_size if max_a_ind_itemsize <= mkl_int_size else mkl_int64_size
+        target_int_size = (
+            mkl_int_size if max_a_ind_itemsize <= mkl_int_size else mkl_int64_size
+        )
         self._ind_dtype = np.dtype(f"i{target_int_size}")
 
         data, indptr, indices = self._validate_matrix_dtypes(A)
@@ -169,7 +174,14 @@ class MKLPardisoSolver:
             HandleClass = _PardisoHandle_int_t
         else:
             HandleClass = _PardisoHandle_long_t
-        self._handle = HandleClass(self._data_dtype, self.shape[0], matrix_type, maxfct=1, mnum=1, msglvl=verbose)
+        self._handle = HandleClass(
+            self._data_dtype,
+            self.shape[0],
+            matrix_type,
+            maxfct=1,
+            mnum=1,
+            msglvl=verbose,
+        )
 
         self._analyze()
         self._factored = False
@@ -188,8 +200,8 @@ class MKLPardisoSolver:
         A : scipy.sparse.spmatrix
             A sparse matrix preferably in a CSR format.
         """
-        #Assumes that the matrix A has the same non-zero pattern and ordering
-        #as the initial A matrix
+        # Assumes that the matrix A has the same non-zero pattern and ordering
+        # as the initial A matrix
 
         if not sp.issparse(A):
             raise TypeError("A is not a sparse matrix.")
@@ -233,8 +245,10 @@ class MKLPardisoSolver:
         Fortran order.
         """
         if b.dtype != self._data_dtype:
-            warnings.warn("rhs does not have the same data type as A",
-                            PardisoTypeConversionWarning)
+            warnings.warn(
+                "rhs does not have the same data type as A",
+                PardisoTypeConversionWarning,
+            )
             b = b.astype(self._data_dtype)
         b = np.atleast_1d(b)
         b_was_1d = b.ndim == 1
@@ -243,16 +257,20 @@ class MKLPardisoSolver:
         if b.ndim != 2:
             raise ValueError(f"b.ndim={b.ndim} must be 1 or 2.")
         if b.shape[0] != self.shape[0]:
-            raise ValueError(f"incorrect length of b, expected {self.shape[0]}, got {b.shape[0]}")
-        b = np.require(b, requirements='F')
+            raise ValueError(
+                f"incorrect length of b, expected {self.shape[0]}, got {b.shape[0]}"
+            )
+        b = np.require(b, requirements="F")
 
         if x is None:
             x = np.empty_like(b)
             x_was_1d = b_was_1d
         else:
-            if(x.dtype!=self._data_dtype):
-                warnings.warn("output does not have the same data type as A",
-                                PardisoTypeConversionWarning)
+            if x.dtype != self._data_dtype:
+                warnings.warn(
+                    "output does not have the same data type as A",
+                    PardisoTypeConversionWarning,
+                )
                 x = x.astype(self._data_dtype)
             x = np.atleast_1d(x)
             x_was_1d = x.ndim == 1
@@ -261,12 +279,15 @@ class MKLPardisoSolver:
             if x.ndim != 2:
                 raise ValueError(f"x.ndim={x.ndim} must be 1 or 2.")
             if x.shape[0] != self.shape[0]:
-                raise ValueError(f"incorrect length of x, expected {self.shape[0]}, got {x.shape[0]}")
-            x = np.require(x, requirements='F')
+                raise ValueError(
+                    f"incorrect length of x, expected {self.shape[0]}, got {x.shape[0]}"
+                )
+            x = np.require(x, requirements="F")
 
         if b.shape[1] != x.shape[1]:
             raise ValueError(
-                f"Inconsistent shapes of right hand side, {b.shape} and output vector, {x.shape}")
+                f"Inconsistent shapes of right hand side, {b.shape} and output vector, {x.shape}"
+            )
 
         if x is b or (x.base is not None and (x.base is b.base)):
             raise ValueError("x and b cannot point to the same memory")
@@ -277,35 +298,35 @@ class MKLPardisoSolver:
         self._handle.set_iparm(11, 2 if transpose else 0)
 
         phase = 33
-        error = self._handle.call_pardiso(phase, self._data, self._indptr, self._indices, b, x)
+        error = self._handle.call_pardiso(
+            phase, self._data, self._indptr, self._indices, b, x
+        )
         if error:
-            raise PardisoError("Solve step error, "+_err_messages[error])
+            raise PardisoError("Solve step error, " + _err_messages[error])
         if x_was_1d:
             x = x[:, 0]
         return x
 
     @property
     def perm(self):
-        """ Fill-reducing permutation vector used inside pardiso.
-        """
+        """Fill-reducing permutation vector used inside pardiso."""
         return np.array(self._handle.perm)
 
     @property
     def iparm(self):
-        """ Parameter options for the pardiso solver.
-        """
+        """Parameter options for the pardiso solver."""
         return np.array(self._handle.iparm)
 
     def _validate_csr_matrix(self, mat):
         if self.matrix_type in [-2, 2, -4, 4, 6]:
             # only grab the upper triangle.
-            mat = sp.triu(mat, format='csr')
+            mat = sp.triu(mat, format="csr")
 
-        if mat.format != 'csr':
+        if mat.format != "csr":
             warnings.warn(
-                "Converting %s matrix to CSR format."% mat.__class__.__name__,
+                "Converting %s matrix to CSR format." % mat.__class__.__name__,
                 PardisoTypeConversionWarning,
-                stacklevel=3
+                stacklevel=3,
             )
             mat = mat.tocsr()
 
@@ -319,13 +340,34 @@ class MKLPardisoSolver:
         indices = np.require(mat.indices, self._ind_dtype, requirements="C")
         return data, indptr, indices
 
-
     def set_iparm(self, i, val):
         if i > 63 or i < 0:
             raise IndexError(f"index {i} is out of bounds for size 64 array")
         if i not in [
-            1, 3, 4, 5, 7, 9, 10, 11, 12, 17, 18, 20, 23,
-            24, 26, 30, 33, 34, 35, 36, 38, 42, 55, 59
+            1,
+            3,
+            4,
+            5,
+            7,
+            9,
+            10,
+            11,
+            12,
+            17,
+            18,
+            20,
+            23,
+            24,
+            26,
+            30,
+            33,
+            34,
+            35,
+            36,
+            38,
+            42,
+            55,
+            59,
         ]:
             raise ValueError(f"cannot set parameter {i} of the iparm array")
 
@@ -338,26 +380,32 @@ class MKLPardisoSolver:
     def _analyze(self):
         phase = 11
         xb_dummy = np.empty([1, 1], dtype=self._data_dtype)
-        error = self._handle.call_pardiso(phase, self._data, self._indptr, self._indices, xb_dummy, xb_dummy)
+        error = self._handle.call_pardiso(
+            phase, self._data, self._indptr, self._indices, xb_dummy, xb_dummy
+        )
         if error:
-            raise PardisoError("Analysis step error, "+_err_messages[error])
+            raise PardisoError("Analysis step error, " + _err_messages[error])
 
     def factor(self):
         phase = 22
         self._factored = False
         xb_dummy = np.empty([1, 1], dtype=self._data_dtype)
-        error = self._handle.call_pardiso(phase, self._data, self._indptr, self._indices, xb_dummy, xb_dummy)
+        error = self._handle.call_pardiso(
+            phase, self._data, self._indptr, self._indices, xb_dummy, xb_dummy
+        )
 
         if error:
-            raise PardisoError("Factor step error, "+_err_messages[error])
+            raise PardisoError("Factor step error, " + _err_messages[error])
 
         self._factored = True
-    
+
     def clear(self):
         phase = -1
         self._factored = False
         xb_dummy = np.empty([1, 1], dtype=self._data_dtype)
-        error = self._handle.call_pardiso(phase, self._data, self._indptr, self._indices, xb_dummy, xb_dummy)
+        error = self._handle.call_pardiso(
+            phase, self._data, self._indptr, self._indices, xb_dummy, xb_dummy
+        )
 
         if error:
-            raise PardisoError("Factor step error, "+_err_messages[error])
+            raise PardisoError("Factor step error, " + _err_messages[error])
